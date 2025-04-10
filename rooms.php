1b404aa812f4048fb61aadf9ad8a483ccddcb6c4
ilyas <ilyasbenmoumen43@example.com>
2025-04-10 17:30:07 +0100

    Ajout 2eme commit

--- rooms.php
+++ rooms.php
@@ -19,7 +19,7 @@
   <div class="container">
     <div class="row">
 
-      <div class="col-lg-3 col-md-12 mb-lg-0 mb-4 ">
+      <div class="col-lg-3 col-md-12 mb-lg-0 mb-4 px-lg-0 ">
         <nav class="navbar navbar-expand-lg navbar-light bg-white rounded shadow">
           <div class="container-fluid flex-lg-column align-items-stretch">
             <h4 class="mt-2">FILTERS</h4>
@@ -68,20 +68,169 @@
       </div>
 
       <div class="col-lg-9 col-md-12 px-4">
-          <div class="card mb-4" >
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img src="..." class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          <div class="card mb-4 border-0 shadow" >
+            <div class="row g-0 p-3 align-items-center">
+              <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
+                <img src="images/rooms/1.jpg" class="img-fluid rounded" alt="...">
+              </div>
+              <div class="col-md-5 px-lg-3 px-md-3 px-0">
+                <h5 class="mb-3">Simple Room Name</h5>
+                <div class="features mb-3">
+                  <h6 class="mb-1">Features</h6>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    2 Rooms
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    1 Bathroom
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    1 Balcon
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    3 Sofa
+                  </span>
+                </div>
+                <div class="facilities mb-3">
+                  <h6 class="mb-1">Facilities</h6>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    Wifi
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    Television
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    AC
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    Room heater
+                  </span>
+                </div>
+                <div class="guests">
+                  <h6 class="mb-1">Guests</h6>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    5 Adultes
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    4 Children
+                  </span>
+                </div>
+              </div>
+              <div class="col-md-2 mt-lg-0 mt-md-0 mt-4 text-center">
+                <h6 class="mb-4">200$ per night</h6>
+                <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Book Now</a>
+                <a href="#" class="btn btn-sm w-100 btn-outline-dark shadow-none">More details</a>
               </div>
             </div>
           </div>
+          <div class="card mb-4 border-0 shadow" >
+            <div class="row g-0 p-3 align-items-center">
+              <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
+                <img src="images/rooms/1.jpg" class="img-fluid rounded" alt="...">
+              </div>
+              <div class="col-md-5 px-lg-3 px-md-3 px-0">
+                <h5 class="mb-3">Simple Room Name</h5>
+                <div class="features mb-3">
+                  <h6 class="mb-1">Features</h6>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    2 Rooms
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    1 Bathroom
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    1 Balcon
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    3 Sofa
+                  </span>
+                </div>
+                <div class="facilities mb-3">
+                  <h6 class="mb-1">Facilities</h6>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    Wifi
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    Television
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    AC
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    Room heater
+                  </span>
+                </div>
+                <div class="guests">
+                  <h6 class="mb-1">Guests</h6>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    5 Adultes
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    4 Children
+                  </span>
+                </div>
+              </div>
+              <div class="col-md-2 text-center">
+                <h6 class="mb-4">200$ per night</h6>
+                <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Book Now</a>
+                <a href="#" class="btn btn-sm w-100 btn-outline-dark shadow-none">More details</a>
+              </div>
+            </div>
           </div>
+          <div class="card mb-4 border-0 shadow" >
+            <div class="row g-0 p-3 align-items-center">
+              <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
+                <img src="images/rooms/1.jpg" class="img-fluid rounded" alt="...">
+              </div>
+              <div class="col-md-5 px-lg-3 px-md-3 px-0">
+                <h5 class="mb-3">Simple Room Name</h5>
+                <div class="features mb-3">
+                  <h6 class="mb-1">Features</h6>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    2 Rooms
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    1 Bathroom
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    1 Balcon
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    3 Sofa
+                  </span>
+                </div>
+                <div class="facilities mb-3">
+                  <h6 class="mb-1">Facilities</h6>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    Wifi
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    Television
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    AC
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    Room heater
+                  </span>
+                </div>
+                <div class="guests">
+                  <h6 class="mb-1">Guests</h6>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    5 Adultes
+                  </span>
+                  <span class="badge bg-light text-dark  text-wrap ">
+                    4 Children
+                  </span>
+                </div>
+              </div>
+              <div class="col-md-2 text-center">
+                <h6 class="mb-4">200$ per night</h6>
+                <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Book Now</a>
+                <a href="#" class="btn btn-sm w-100 btn-outline-dark shadow-none">More details</a>
+              </div>
+            </div>
+          </div>
+          
       </div>
 
     </div>
