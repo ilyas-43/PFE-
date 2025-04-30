b387209314552950cd59296f3d1fbac7fd394d34
ilyas <ilyasbenmoumen43@example.com>
2025-04-30 15:27:59 +0100

    Ton message de commit

---
+++ admin/ajax/settings_crud.php
@@ -0,0 +1,36 @@
+<?php 
+    require('../inc/db-config.php');
+    require('../inc/essentials.php');
+    adminlogin();
+
+    if(isset($_POST['get_general']))
+    {
+        $q = "SELECT * FROM `settings` WHERE `sr_no`=?";
+        $values = [1];
+        $res = select($q,$values,"i");
+        $data = mysqli_fetch_assoc($res);
+        $json_data = json_encode($data);
+        echo $json_data;
+    }
+
+    if(isset($_POST['upd_general']))
+    {
+        $frm_data = filteration($_POST);
+
+        $q = "UPDATE `settings` SET `site_about`=? WHERE `sr_no`=?";
+        $values = [$frm_data['site_about'],1];
+        $res = update($q,$values,'si');
+        echo $res;
+    }
+
+    if(isset($_POST['upd_shutdown']))
+    {
+        $frm_data = ($_POST['upd_shutdown']==0) ? 1 : 0;
+
+        $q = "UPDATE `settings` SET `shutdown`=? WHERE `sr_no`=?";
+        $values = [$frm_data,1];
+        $res = update($q,$values,'ii');
+        echo $res;
+    }
+
+?>
--- projet fdt.sql
+++ projet fdt.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : sam. 26 avr. 2025 à 12:13
--- Version du serveur : 10.4.28-MariaDB
--- Version de PHP : 8.0.28
+-- Généré le : mer. 30 avr. 2025 à 16:25
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `hbwebsite`
+-- Base de données : `projet fdt`
 --
 
 -- --------------------------------------------------------
@@ -40,6 +40,25 @@ CREATE TABLE `admin-cred` (
 INSERT INTO `admin-cred` (`sr-no`, `admin-name`, `admin-pass`) VALUES
 (1, 'tjwebdev', '12345');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `settings`
+--
+
+CREATE TABLE `settings` (
+  `sr_no` int(11) NOT NULL,
+  `site_about` varchar(250) NOT NULL,
+  `shutdown` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `settings`
+--
+
+INSERT INTO `settings` (`sr_no`, `site_about`, `shutdown`) VALUES
+(1, 'ROYAL EMIRALD', 0);
+
 --
 -- Index pour les tables déchargées
 --
@@ -51,6 +70,12 @@ ALTER TABLE `admin-cred`
   ADD PRIMARY KEY (`sr-no`);
 
 --
+-- Index pour la table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`sr_no`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
@@ -59,6 +84,12 @@ ALTER TABLE `admin-cred`
 --
 ALTER TABLE `admin-cred`
   MODIFY `sr-no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `sr_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
