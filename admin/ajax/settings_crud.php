40830e8db052a47ea969039c1b00e6ffdf4a0dfd
mohamed lamhamdi <mohamed@gmail.com>
2025-05-18 01:28:32 +0200

    8VDL

--- admin/ajax/settings_crud.php
+++ admin/ajax/settings_crud.php
@@ -43,4 +43,14 @@
         echo $json_data;
     }
 
+     if(isset($_POST['upd_contacts']))
+    {
+        $frm_data = filteration($_POST);
+
+        $q = "UPDATE `contact_details` SET `address`=?,`gmap`=?,`pn1`=?,`pn2`=?,`email`=?,`fb`=?,`insta`=?,`tw`=?,`iframe`=? WHERE `sr_no`=?";
+        $values = [$frm_data['address'],$frm_data['gmap'],$frm_data['pn1'],$frm_data['pn2'],$frm_data['email'],$frm_data['fb'],$frm_data['insta'],$frm_data['tw'],$frm_data['iframe'],1];
+        $res = update($q,$values,'sssssssssi');
+        echo $res;
+    }
+
 ?>
